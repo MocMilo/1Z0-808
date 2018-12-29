003875b0883a027da324b45814819d1e69cc573a
mocmilo <mmociewicz@gmail.com>
2018-12-29 15:13:42 +0100

    Added assessment test.

---
+++ src/main/java/com/mocmilo/_C.java
@@ -0,0 +1,11 @@
+package com.mocmilo;
+
+public class _C {
+    private static int $;
+
+    public static void main(String[] main) {
+        String a_b;
+        System.out.println($);
+       // System.out.printf(a_b);     // this line will not compile (local variable)
+    }
+}
